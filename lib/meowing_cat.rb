d56fc4362a855356defa5a480e65055411e9b8ee
github username <github email address>
2019-01-09 11:46:34 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,22 @@
-## code your solution here. 
+## code your solution here.
+
+class Cat
+  attr_accessor :name
+
+def meow
+  puts "meow!"
+end
+end
+
+
+
+
+
+
+
+# class  = Struct.new(:attr_names) do
+  #def method_name
+
+#  end
+
+#end
